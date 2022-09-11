2b976c1a4783e8b0e06adce3145cea1246665c6e
v1adk0 <yurtaev2846@gmail.com>
2022-09-11 13:25:13 +0400

    Task2              [3, 4, 2, 7], 10 -> [3, 7] - вывести пару именно в скобках, которые дают сумму - 10

--- TaskTwo/Main.java
+++ TaskTwo/Main.java
@@ -5,7 +5,14 @@ import java.util.Arrays;
 
 public class Main {
 
+    public static void main(String[] args) {
+//        int[] testArr = {3, 4, 2, 7};
+//        sumOf(testArr, 10);
+    }
+
        static void sumOf(int[] arr, int sum) {
+           if (arr == null) arr = new int[0];
+
            int[] res = new int[2];
 
            for (int i : arr) {
