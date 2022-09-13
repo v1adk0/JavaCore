3f5b5b6a627762cb0c360031aef8b4f604df6c80
v1adk0 <yurtaev2846@gmail.com>
2022-09-13 21:29:00 +0400

    Task2             Вывести пару именно в скобках, которые дают сумму - 10

--- TaskTwo/Main.java
+++ TaskTwo/Main.java
@@ -2,26 +2,38 @@ package TaskTwo;
 
 import java.util.Arrays;
 
-
 public class Main {
 
     public static void main(String[] args) {
-//        int[] testArr = {3, 4, 2, 7};
-//        sumOf(testArr, 10);
+        int[] testArr = {3, 4, 2, 7};
+        sumOf(testArr, 10);
     }
 
-       static void sumOf(int[] arr, int sum) {
-           if (arr == null) arr = new int[0];
+static void sumOf(int[] arr, int sum) {
+    if (arr == null) {
+        arr = new int[0];
+    }
+        int first =0;
+        int last = arr.length - 1;
 
-           int[] res = new int[2];
+       while (first != last) {
 
-           for (int i : arr) {
-               for (int j = arr.length - 1; j > 0; j--) {
-                   if ((i + arr[j] == sum)) {
-                       res = new int[]{i, arr[j]};
-                   }
-               }
+           int[] res;
+           int sumCount = arr[first] + arr[last];
+
+           if (sumCount < sum) {
+               first++;
+           } else if (sumCount > sum) {
+               last--;
+           } else {
+               res = new int[]{arr[first], arr[last]};
+               System.out.println(Arrays.toString(res));
+               break;
            }
-           System.out.println(Arrays.toString(res));
-       }
+        }
+
 }
+}
+
+
+
